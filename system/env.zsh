d1462872a084bb9fe6576d142f6dca72d138c253
Patrik Nordebo <patrik.nordebo@atex.com>
2013-01-07 10:37:59 +0100

    Tweaked path, got rid of JRebel and Yourkit in Maven, tweaked DYLD_LIBRARY_PATH to include yourkit JNI library

--- system/env.zsh
+++ system/env.zsh
@@ -1,12 +1,12 @@
 source ~/.svnvars
 
-export PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH:/usr/games:$HOME/bin/scripts
+export PATH=/Applications/Emacs.app/Contents/MacOS/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH:/usr/games:$HOME/bin/scripts
 export EDITOR=emacsclient
 export PAGER=less
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
 
 export REBEL_HOME=/Applications/Development/JRebel
-export MAVEN_OPTS="-Xmx2048m -noverify -Xbootclasspath/p:$REBEL_HOME/jrebel-bootstrap.jar:$REBEL_HOME/jrebel.jar -XX:MaxPermSize=512m  -agentlib:yjpagent"
+export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m"
 
 export LESSCHARSET='latin1'
 export LANG='sv_SE'
@@ -15,7 +15,7 @@ export LC_LANG='sv_SE'
 
 # Prestige build stuff
 export PRBUILDENV_MAIN=/opt/src/prestige/build-env
-export DYLD_LIBRARY_PATH=/opt/prestige/lib:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=/Applications/Development/YourKit.app/bin/mac/:/opt/prestige/lib:$DYLD_LIBRARY_PATH
 export PATH=$PATH:/opt/prestige/bin
 
 launchctl setenv PATH $PATH
