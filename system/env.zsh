fdd67fb73e2dcaa6196391f160e90a6d02ae83ec
patrikn <patrik@nordebo.com>
2018-04-10 17:09:52 +0200

    Removed some stuff I no longer need, and made the Java enrivonment conditional on Java being installed

--- system/env.zsh
+++ system/env.zsh
@@ -1,19 +1,15 @@
-export PATH=/Applications/Emacs.app/Contents/MacOS/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH:/usr/games:$HOME/bin/scripts
+export PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH:/usr/games:$HOME/bin/scripts
 export EDITOR=emacsclient
 export PAGER=less
-export JAVA_HOME=`/usr/libexec/java_home -v 1.7.0`
-
-export REBEL_HOME=/Applications/Development/JRebel
-export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=4096m"
+JAVA_HOME=`/usr/libexec/java_home -F -v 1.7.0 2>/dev/null`
+if [[ -n "$JAVA_HOME" ]]; then
+   export JAVA_HOME
+   export REBEL_HOME=/Applications/Development/JRebel
+   export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=4096m"
+fi
 
 export LESSCHARSET='latin1'
 export LANG='sv_SE'
 export LC_LANG='sv_SE'
-
-
-# Prestige build stuff
-export PRBUILDENV_MAIN=/opt/src/prestige/build-env
-export DYLD_LIBRARY_PATH=/Applications/Development/YourKit.app/bin/mac/:/opt/prestige/lib:$DYLD_LIBRARY_PATH
-export PATH=$PATH:/opt/prestige/bin
 
 launchctl setenv PATH $PATH
