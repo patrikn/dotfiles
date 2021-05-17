5a0b76b6c8f25508da76704b664aa273e7a5e388
Patrik Nordebo <patrik.nordebo@nordkap.se>
2021-05-17 11:49:58 +0200

    Some Python stuff plus en_US locale

--- system/env.zsh
+++ system/env.zsh
@@ -1,4 +1,4 @@
-export PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH:/usr/games:$HOME/bin/scripts
+export PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH:/usr/games:$HOME/bin/scripts:$HOME/.dotnet/tools:$HOME/.poetry/bin
 export EDITOR=emacsclient
 export PAGER=less
 JAVA_HOME=`/usr/libexec/java_home -F -v 1.7.0 2>/dev/null`
@@ -9,7 +9,13 @@ if [[ -n "$JAVA_HOME" ]]; then
 fi
 
 export LESSCHARSET='latin1'
-export LANG='sv_SE'
-export LC_LANG='sv_SE'
+export LANG='en_US'
+export LC_LANG='en_US'
+export LC_CTYPE='UTF-8'
 
 launchctl setenv PATH $PATH
+
+# pyenv
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
